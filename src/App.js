bd97ab3ae7be31b155491b9d39bba980eff25ebf
sebastian-costa <sebacosta8822@gmail.com>
2022-05-25 20:44:15 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import'bootstrap/dist/css/bootstrap.css';
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>
+          <img src={logo} width="40px"/>
+          Logo
+        </Navbar.Brand>
+        <Nav>
+          <NavDropdown title="Productos">
+          <NavDropdown.Item href="#">Producto 1</NavDropdown.Item>
+          <NavDropdown.Item href="#">Producto 2</NavDropdown.Item>
+          <NavDropdown.Item href="#">Producto 3</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="contacto">Contacto</Nav.Link>
+          <Nav.Link href="productos">Nosotros</Nav.Link>
+        </Nav>
+
+      </Navbar>
+    
     </div>
   );
 }
---
+++ src/components/navBar.jsx
@@ -0,0 +1,12 @@
+
+
+
+function navBar() {
+    return (
+      <div>
+        <h1>Ecommerce</h1>
+      </div>
+    );
+  }
+
+  export default navBar;
