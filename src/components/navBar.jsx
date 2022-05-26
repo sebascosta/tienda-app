9dc10c5eed02fe33cab9e4687568f37fe77eb00a
sebastian-costa <sebacosta8822@gmail.com>
2022-05-25 21:01:52 -0300

    navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import'bootstrap/dist/css/bootstrap.css';
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>
+          <img src={logo} width="40px"/>
+          Logo
+        </Navbar.Brand>
+        <Nav>
+          <NavDropdown title="Productos">
+          <NavDropdown.Item href="#">Producto 1</NavDropdown.Item>
+          <NavDropdown.Item href="#">Producto 2</NavDropdown.Item>
+          <NavDropdown.Item href="#">Producto 3</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="contacto">Contacto</Nav.Link>
+          <Nav.Link href="productos">Nosotros</Nav.Link>
+        </Nav>
 
+      </Navbar>
+    
+    </div>
+  );
+}
 
-function navBar() {
-    return (
-      <div>
-        <h1>Ecommerce</h1>
-      </div>
-    );
-  }
-
-  export default navBar;
+export default App;
